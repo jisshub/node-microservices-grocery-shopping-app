48f7b254b10e79bfeb54b9ec435032043a797f77
jisshub <jissmon476.com>
2022-05-13 18:48:39 +0530

    changes

--- customer/src/database/connection.js
+++ customer/src/database/connection.js
@@ -7,7 +7,7 @@ module.exports = async() => {
         await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            // useCreateIndex: true
         });
         console.log('Db Connected');
         
